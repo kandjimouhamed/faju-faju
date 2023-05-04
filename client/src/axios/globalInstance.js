cdf6afbda6bee55935a91b3a189a897807c2a460
mouhamed kandji <kandjimouhamed@gmail.com>
2023-05-04 19:01:00 +0000

    remetre api prod

--- client/src/axios/globalInstance.js
+++ client/src/axios/globalInstance.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const instance = axios.create({
     // baseURL: "https://rendez-vous-app-api.herokuapp.com/api"
-    baseURL: "http://localhost:5550/api"
-    // baseURL: "https://faju-faju-production.up.railway.app/api"
+    // baseURL: "http://localhost:5550/api"
+    baseURL: "https://faju-faju-production.up.railway.app/api"
     
 })
 
