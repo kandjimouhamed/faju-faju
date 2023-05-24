a1d28a37a5003d14adc605e143a0c4e71f4008f2
Alioune Gningue <gninguealiou102@gmail.com>
2023-05-24 21:34:43 +0200

    ajouter

--- client/src/axios/globalInstance.js
+++ client/src/axios/globalInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: "https://faju-faju-production.up.railway.app/api"
-    baseURL: "http://localhost:5550/api"
+    baseURL: "https://faju-production.up.railway.app/api"
+    // baseURL: "http://localhost:5550/api"
     
 })
 
