5a40fa78c214c9a14680b39b5bc6a72018fbf21d
Alioune Gningue <gninguealiou102@gmail.com>
2023-05-31 23:57:06 +0200

    ajouter de la deploy patient modifier night

--- client/src/axios/globalInstance.js
+++ client/src/axios/globalInstance.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 const instance = axios.create({
     baseURL: "https://faju-qt60.onrender.com/api"
     // baseURL: "http://localhost:5550/api"
-    
 })
 
 export default instance
